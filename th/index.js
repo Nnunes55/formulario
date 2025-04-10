bb9d4d9e63d39ab89db238b7bc2762fda5f6c76e
Nathan Nunes <nathannunes@MacBook-Air-de-Nathan.local>
2025-04-10 15:33:25 -0300

    primeiro commit

---
+++ th/index.js
@@ -0,0 +1,18 @@
+let x, y, z;
+x = 10;
+y = 24;
+z = x + y;
+a = 34;
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragrafo mudado.";
+  }
+
+function myFunction2() {
+  document.getElementById("demo").innerHTML = '<h2>vai ser mudado</h2>';
+}
+
+function myfunction3() {
+  window.print();
+}
+console.log(a);
